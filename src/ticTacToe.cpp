2c4a66daf6248d01316dd6cde5c0de39b4a15126
Andrey Golyachenko <leswar@yandex.ru>
2023-06-21 17:57:20 +0500

    Corrections

--- src/ticTacToe.cpp
+++ src/ticTacToe.cpp
@@ -26,10 +26,10 @@ int who_did_win_in_diagonal(char tic_tac_toe[][3])
 		(tic_tac_toe[0][2] == tic_tac_toe[1][1] && tic_tac_toe[0][2] == tic_tac_toe[2][0]))
 		winnerIs = 1;
 
-	if ((tic_tac_toe[0][0] == 'Y' || tic_tac_toe[0][0] == 'y') &&
+	if ((tic_tac_toe[0][0] == 'O' || tic_tac_toe[0][0] == 'o') &&
 		(tic_tac_toe[0][0] == tic_tac_toe[1][1] && tic_tac_toe[0][0] == tic_tac_toe[2][2]))
 		winnerIs = 2;
-	else if ((tic_tac_toe[0][2] == 'Y' || tic_tac_toe[0][2] == 'y') &&
+	else if ((tic_tac_toe[0][2] == 'O' || tic_tac_toe[0][2] == 'o') &&
 		(tic_tac_toe[0][2] == tic_tac_toe[1][1] && tic_tac_toe[0][2] == tic_tac_toe[2][0]))
 		winnerIs = 2;
 
@@ -45,7 +45,7 @@ int who_did_win_in_col(char tic_tac_toe[][3])
 		for (int j = 0; j < 3; ++j)
 		{
 			if (tic_tac_toe[j][i] == 'X' || tic_tac_toe[j][i] == 'x') ++x;
-			if (tic_tac_toe[j][i] == 'Y' || tic_tac_toe[j][i] == 'y') ++y;
+			if (tic_tac_toe[j][i] == 'O' || tic_tac_toe[j][i] == 'o') ++y;
 		}
 		if (x == 3) winnerIs = 1;
 		else if (y == 3) winnerIs = 2;
@@ -68,7 +68,7 @@ int who_did_win(char tic_tac_toe[][3])
 				++countXAndY;
 				++x;
 			}
-			if (tic_tac_toe[i][j] == 'Y' || tic_tac_toe[i][j] == 'y') 
+			if (tic_tac_toe[i][j] == 'O' || tic_tac_toe[i][j] == 'o') 
 			{
 				++countXAndY;
 				++y;
@@ -96,14 +96,14 @@ bool logic(char tic_tac_toe[][3], int coorX, int coorY, char input_X_or_Y)
 		for (int j = 0; j < 3; ++j)
 		{
 			if (tic_tac_toe[i][j] == 'X' || tic_tac_toe[i][j] == 'x') ++x;
-			if (tic_tac_toe[i][j] == 'Y' || tic_tac_toe[i][j] == 'y') ++y;
+			if (tic_tac_toe[i][j] == 'O' || tic_tac_toe[i][j] == 'o') ++y;
 		}
 	}
 
 	if (x > y && (input_X_or_Y == 'X' || input_X_or_Y == 'x')) return false;
-	else if ((input_X_or_Y == 'X' || input_X_or_Y == 'Y' || input_X_or_Y == 'x' || input_X_or_Y == 'y')
+	else if ((input_X_or_Y == 'X' || input_X_or_Y == 'O' || input_X_or_Y == 'x' || input_X_or_Y == 'o')
 		&& tic_tac_toe[coorX - '0'][coorY - '0'] != '_') return false;
-	else if (x == y && (input_X_or_Y == 'Y' || input_X_or_Y == 'y')) return false;
+	else if (x == y && (input_X_or_Y == 'O' || input_X_or_Y == 'o')) return false;
 
 	return true;
 }
@@ -113,14 +113,14 @@ bool correct_input_data(char input_X_or_Y, char coorX, char coorY)
 	if (coorX - '0' < 0 || coorX - '0' > 2 || coorY - '0' < 0 || coorY - '0' > 2) return false;
 
 	if (input_X_or_Y == 'X' || input_X_or_Y == 'x' ||
-		input_X_or_Y == 'Y' || input_X_or_Y == 'y') return true;
+		input_X_or_Y == 'O' || input_X_or_Y == 'o') return true;
 
 	return false;
 }
 
 int main()
 {
-	char input_X_or_Y = ' ';
+	char input_X_or_O = ' ';
 	bool gamer_X = false, gamer_Y = false, nobody = false;
 	char coorX, coorY;
 	const int ROW = 3, COL = 3;
@@ -133,19 +133,19 @@ int main()
 		std::cout << "Enter to coordinates: ";
 		std::cin >> coorX >> coorY;
 		
-		std::cout << "Enter to element(x or y): ";
-		std::cin >> input_X_or_Y;
+		std::cout << "Enter to element(x or o): ";
+		std::cin >> input_X_or_O;
 
-		if (!correct_input_data(input_X_or_Y, coorX, coorY))
-			std::cout << "Error! You can enter only coordinates: 0 - 3. Element: x or y(X or Y). \nTry again! " << std::endl;
-		else if (!logic(tic_tac_toe, coorX, coorY, input_X_or_Y))
+		if (!correct_input_data(input_X_or_O, coorX, coorY))
+			std::cout << "Error! You can enter only coordinates: 0 - 3. Element: x or y(x or o). \nTry again! " << std::endl;
+		else if (!logic(tic_tac_toe, coorX, coorY, input_X_or_O))
 		{
 			std::cout << "You cannot enter these coordinates, because this field is already full. " << 
 				"Or is it another player's turn. Try again!" << std::endl;
 		}
 		else
 		{
-			tic_tac_toe[coorX - '0'][coorY - '0'] = input_X_or_Y;
+			tic_tac_toe[coorX - '0'][coorY - '0'] = input_X_or_O;
 			int winner = who_did_win(tic_tac_toe);
 			draw(tic_tac_toe);
 			switch (winner)
@@ -156,7 +156,7 @@ int main()
 				break;
 			case 2:
 				gamer_Y = true;
-				std::cout << "Winner is GAMER Y!!! " << std::endl;
+				std::cout << "Winner is GAMER O!!! " << std::endl;
 				break;
 			case 3:
 				nobody = true;
